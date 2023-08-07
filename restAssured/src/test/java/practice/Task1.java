5ac66685d464591ef5fc8c22bb6fdaaa5a6e06fe
karthikmshetty <karthikmshetty108@gmail.com>
2023-08-07 11:16:36 +0530

    .

---
+++ restAssured/src/test/java/practice/Task1.java
@@ -0,0 +1,35 @@
+package practice;
+
+import java.io.FileInputStream;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.json.simple.JSONObject;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.google.gson.JsonObject;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import junit.framework.Assert;
+
+import static io.restassured.RestAssured.*;
+
+public class Task1 {
+	
+	public void project()
+	{
+
+	}
+
+	    
+	}
+	
+	
+
